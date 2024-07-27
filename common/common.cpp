35513959759ac3c3102d65271cd091e0dcb5e02f
Jerboa-app <dev@jerboa.app>
2024-07-27 11:38:55 +0100

    use env

--- common/common.cpp
+++ common/common.cpp
@@ -6,7 +6,6 @@ std::string fixedLengthNumber(double x, unsigned length)
     std::string dtrunc(length,' ');
     for (unsigned c = 0; c < dtrunc.length(); c++)
     {
-
         if (c >= d.length())
         {
             dtrunc[c] = '0';
